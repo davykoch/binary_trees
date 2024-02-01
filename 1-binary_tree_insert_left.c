b5be506b2384d54ef254b0b02bbd36c299c9cdb7
Annsrutto <annsrutto567@gmail.com>
2024-02-02 01:20:18 +0300

    left node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,21 +14,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	binary_tree_t *newNode = binary_tree_node(NULL, value);
+	binary_tree_t *newNode = binary_tree_node(parent, value);
 
 	if (newNode == NULL)
 	{
 		return (NULL);
 	}
+	newNode->left = parent->left;
 
-	if (parent->left != NULL)
-	{
-		newNode->left = parent->left;
-		parent->left->parent = newNode;
-	}
-
+	if (newNode->left != NULL)
+		newNode->left->parent = newNode;
 	parent->left = newNode;
-	newNode->parent = parent;
 
 	return (newNode);
 }
