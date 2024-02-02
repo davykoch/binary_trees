8315c1b555efbea42f0dfe7ce71791ae918b7d14
davykoch <koechdavis24@gmail.com>
2024-02-02 03:58:39 +0300

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,12 +8,13 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height = 0;
+	size_t right_height = 0;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	size_t left_height = 0, right_height = 0;
 
 	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
