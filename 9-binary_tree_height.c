4f02921a2a15c83355f6862f92f1ac9f27e8334f
Annsrutto <annsrutto567@gmail.com>
2024-02-02 03:27:38 +0300

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,11 +15,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	size_t left_height = 0, right_height = 0;
 
-	if (tree->left != NULL || tree->right != NULL)
+	if (tree->left == NULL && tree->right == NULL)
 	{
-		left_height = binary_tree_height(tree->left);
-		right_height = binary_tree_height(tree->right);
-		return ((left_height > right_height ? left_height : right_height) + 1);
+		return (0);
 	}
-	return (0);
+
+	if (tree->left != NULL)
+	{
+		left_height = binary_tree_height(tree->left) + 1;
+	}
+	if (tree->right != NULL)
+	{
+		right_height = binary_tree_height(tree->right) + 1;
+	}
+
+	return (left_height > right_height ? left_height : right_height);
 }
+
